5857a62e3571595b272836a98429e06a554e5605
larisachristie <larisachristie@gmail.com>
2020-06-19 19:59:10 +0200

    Updates

---
+++ precache-manifest.a9df7d6aafb1e83184356cb7a66c0c92.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16b9d214edcc2f5e9bdc7d8f6d811f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c78732ecb6d20a631b3",
+    "url": "./static/css/main.28053328.chunk.css"
+  },
+  {
+    "revision": "dfdecd853e4b2d2b370d",
+    "url": "./static/js/2.65131da1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.65131da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c78732ecb6d20a631b3",
+    "url": "./static/js/main.174074d4.chunk.js"
+  },
+  {
+    "revision": "33c4951af195f96d65f3",
+    "url": "./static/js/runtime-main.68e5d44a.js"
+  },
+  {
+    "revision": "07af22f0deab457c6755eea60d6b8ed8",
+    "url": "./static/media/bed.07af22f0.svg"
+  },
+  {
+    "revision": "c5d27ac4f39ff5b2920780f356b405ce",
+    "url": "./static/media/bicycle.c5d27ac4.svg"
+  },
+  {
+    "revision": "ad4c27db5635d20c25def206fdb05fd4",
+    "url": "./static/media/door.ad4c27db.svg"
+  },
+  {
+    "revision": "267b6c5b514e7f90f02af5044e84fc11",
+    "url": "./static/media/escooter.267b6c5b.svg"
+  },
+  {
+    "revision": "cecb619be7c08e8e44930047900637f4",
+    "url": "./static/media/logoA.cecb619b.svg"
+  },
+  {
+    "revision": "f92762a270d4259ebe167d559d739101",
+    "url": "./static/media/railwayTrackGauge.f92762a2.svg"
+  },
+  {
+    "revision": "7fe48f6b846acab2abe7a0e49d1fb5a9",
+    "url": "./static/media/trolley.7fe48f6b.svg"
+  },
+  {
+    "revision": "75fc27f5f4b2a17ce82bc0d0e290cbf4",
+    "url": "./static/media/warriorPose.75fc27f5.svg"
+  }
+]);
